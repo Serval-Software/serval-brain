ac88d1ccdacbd318dfdb0c86181df653245339ec
Romain Vacheret <romain.vacheret@gmail.com>
2021-12-02 22:05:01 +0100

    chore: initial commit

---
+++ src/main/java/com/serval/servalbrain/ServalBrainApplication.java
@@ -0,0 +1,13 @@
+package com.serval.servalbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServalBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServalBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serval/servalbrain/ServalBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serval.servalbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServalBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
